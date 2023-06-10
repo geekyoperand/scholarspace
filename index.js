3c4f650718c42f9ca3d365641953da93654f6eb9
Nitin Malhotra <nitinmalhotra2019.nm@gmail.com>
2023-06-11 02:06:46 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const connectToMongo = require('./db');
-const app = express()
+const app = express();
 const port = 5000;
 
 connectToMongo();
 
-app.use(express.json())
-app.use('/api', require('./routes/subscriptions'))
+app.set('view engine', 'ejs'); // Set EJS as the view engine
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', require('./routes/subscriptions'));
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.get('/', (req, res) => {
+  res.render('index'); // Render the 'index.ejs' file
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
